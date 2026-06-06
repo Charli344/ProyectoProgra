c63bfc0fd548f5020f4a8c5dadc4d04ac6eeadcf
Dilany <dpamenunez1119@gmail.com>
2026-06-06 12:19:56 -0600

    definicion de metodos personalizados en respository

--- Huellitas/src/main/java/ucr/ac/cr/Huellitas/repository/AdoptionJpaRepository.java
+++ Huellitas/src/main/java/ucr/ac/cr/Huellitas/repository/AdoptionJpaRepository.java
@@ -1,7 +1,15 @@
 package ucr.ac.cr.Huellitas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ucr.ac.cr.Huellitas.model.Adoption;
+import ucr.ac.cr.Huellitas.model.AdoptionStatus;
 
+import java.util.List;
+
+@Repository
 public interface AdoptionJpaRepository extends JpaRepository<Adoption, Integer> {
+
+    List<Adoption> findByUserId(Integer userId);
+    List<Adoption> findByStatus(AdoptionStatus status);
 }
--- Huellitas/src/main/java/ucr/ac/cr/Huellitas/service/AdoptionService.java
+++ Huellitas/src/main/java/ucr/ac/cr/Huellitas/service/AdoptionService.java
@@ -1,4 +1,22 @@
 package ucr.ac.cr.Huellitas.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ucr.ac.cr.Huellitas.repository.AdoptionJpaRepository;
+import ucr.ac.cr.Huellitas.repository.PetJpaRepository;
+import ucr.ac.cr.Huellitas.repository.UserJpaRepository;
+
+@Service
 public class AdoptionService {
+
+    @Autowired
+    private AdoptionJpaRepository repository;
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @Autowired
+    private PetJpaRepository petJpaRepository;
+
+
 }
